b989ed3c054d71f1859e18d3a1e442e43fe0c196
joseAngel33 <163841769+joseAngel33@users.noreply.github.com>
2024-06-21 23:40:49 +0200

    4-ejercicios

--- src/js/index.js
+++ src/js/index.js
@@ -47,6 +47,13 @@ window.addEventListener('resize', windowsWhith);
 
 // - Crea un p con el texto "esperando entrada de teclado" al pulsar cualquier tecla deberá poner qué tecla has pulsado.
 
+document.addEventListener('DOMContentLoaded', event => {
+  document.addEventListener('keydown', event => {
+    const paragraf = document.getElementById('text');
+    paragraf.textContent = `Has pulsado: ${event.key}`;
+  });
+});
+
 // - Crea un array con 5 palabras. Añade un h2 a tu HTML y ponle un id. Añade dos botones con el texto previus y next respectívamente. Haz que los botones cambien el texto del h2 con las palabras del array, cuando llegues a la última volverás a la primera si pulsas next y cuando estés en la primera podrás volver a la última dandole a previous.
 
 // - Repite el mismo ejercicio pero esta vez desactiva el botón previous si estás en el primer elemento y el botón next si estás en el último
